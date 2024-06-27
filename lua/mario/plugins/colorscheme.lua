d5bce79b79c6b64bffd1c61776c1f0b282be6e12
tuhao <tuhao@weipie.com>
2024-06-27 17:54:49 +0800

    feat: change colorscheme to darcula, add conform to formate file content

--- lua/mario/plugins/colorscheme.lua
+++ lua/mario/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
 return {
 	-- add gruvbox
-	{ "ellisonleao/gruvbox.nvim" },
-	  
+	-- { "ellisonleao/gruvbox.nvim" },
+	--   {
+	--		"LazyVim/LazyVim",
+	--		opts = {
+	--			colorscheme = "darcula",
+	--		},
+	--	},
+	-- add darcula-dark
+	--	"xiantang/darcula-dark.nvim",
+	--	dependencies = {
+	--        "nvim-treesitter/nvim-treesitter",
+	--        "nvim-treesitter/nvim-treesitter-textobjects"
+	--    },
 	-- Configure LazyVim to load gruvbox
+	--	{
+	--        "doums/darcula",
+	--        config = function()
+	--            vim.cmd([[colorscheme darcula]])
+	--        end
+	-- }
+	--
+
+	-- add darcula
+	{ "doums/darcula" },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
-	   },
-	}
+			colorscheme = "darcula",
+		},
+	},
 }
---
+++ lua/mario/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+	-- 配置 conform.nvim 插件
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			-- 配置 Conform 插件
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					python = { "isort", "black" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettierd", "prettier" } },
+				},
+				-- to format on save
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+			})
+		end,
+	},
+}
