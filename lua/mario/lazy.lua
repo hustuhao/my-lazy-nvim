e75cb370ffb7284961ab7eb814637e24e6d5bb9d
tuhao <tuhao@weipie.com>
2024-07-02 18:55:12 +0800

    feat: add nvim-dap nvim-lspconfig which-ley

--- lua/mario/lazy.lua
+++ lua/mario/lazy.lua
@@ -1,21 +1,32 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-        lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{import = "mario.plugins"}}, {
-    checker = {
-        enabled = false -- disable check for plugin updates
-    },
-    change_detection = {
-        enabled = false -- disable check for config file changes
-    }
+require("lazy").setup({ { import = "mario.plugins" } }, {
+	checker = {
+		enabled = false, -- disable check for plugin updates
+	},
+	change_detection = {
+		enabled = false, -- disable check for config file changes
+	},
 })
 
+require("lazy").setup({ { import = "mario.config" } }, {})
 
-require("lazy").setup({{import = "mario.config"}}, {})
+-- lang python
+-- require("lazy").setup({
+--   spec = {
+--     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+--     { import = "lazyvim.plugins.extras.lang.python" },
+--     { import = "plugins" },
+--   },
+-- })
---
+++ lua/mario/plugins/nvim-lspconfig.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+			-- 配置 pyright 或 pylsp
+			lspconfig.pyright.setup({
+				--capabilities = capabilities,
+				--on_attach = function(client, bufnr)
+				-- 设置键位映射，比如代码导航和补全
+				--	vim.api.nvim_buf_set_keymap(
+				--		bufnr,
+				--		"n",
+				--		"gd",
+				--		"<Cmd>lua vim.lsp.buf.definition()<CR>",
+				--		{ noremap = true, silent = true }
+				--	)
+				--	vim.api.nvim_buf_set_keymap(
+				--		bufnr,
+				--		"n",
+				--		"gr",
+				--		"<Cmd>lua vim.lsp.buf.references()<CR>",
+				--		{ noremap = true, silent = true }
+				--	)
+				--end,
+			})
+		end,
+	},
+}
