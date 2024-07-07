2622c7608918547a39dda6623eeab214a170a7b6
tuhao <tuhao@weipie.com>
2024-07-07 16:33:55 +0800

    feat: add mason, bashls

---
+++ lua/mario/plugins/mason-lspconfig.lua
@@ -0,0 +1,11 @@
+return {
+
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "clangd", "pyright", "bashls", "lua_ls", "vimls", "jdtls" },
+		})
+	end,
+}
--- lua/mario/plugins/nvim-lspconfig.lua
+++ lua/mario/plugins/nvim-lspconfig.lua
@@ -7,26 +7,26 @@ return {
 			capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 			-- 配置 pyright 或 pylsp
-			lspconfig.pyright.setup({
-				--capabilities = capabilities,
-				--on_attach = function(client, bufnr)
-				-- 设置键位映射，比如代码导航和补全
-				--	vim.api.nvim_buf_set_keymap(
-				--		bufnr,
-				--		"n",
-				--		"gd",
-				--		"<Cmd>lua vim.lsp.buf.definition()<CR>",
-				--		{ noremap = true, silent = true }
-				--	)
-				--	vim.api.nvim_buf_set_keymap(
-				--		bufnr,
-				--		"n",
-				--		"gr",
-				--		"<Cmd>lua vim.lsp.buf.references()<CR>",
-				--		{ noremap = true, silent = true }
-				--	)
-				--end,
-			})
+			--lspconfig.pyright.setup({
+			--capabilities = capabilities,
+			--on_attach = function(client, bufnr)
+			-- 设置键位映射，比如代码导航和补全
+			--	vim.api.nvim_buf_set_keymap(
+			--		bufnr,
+			--		"n",
+			--		"gd",
+			--		"<Cmd>lua vim.lsp.buf.definition()<CR>",
+			--		{ noremap = true, silent = true }
+			--	)
+			--	vim.api.nvim_buf_set_keymap(
+			--		bufnr,
+			--		"n",
+			--		"gr",
+			--		"<Cmd>lua vim.lsp.buf.references()<CR>",
+			--		{ noremap = true, silent = true }
+			--	)
+			--end,
+			--})
 		end,
 	},
 }
