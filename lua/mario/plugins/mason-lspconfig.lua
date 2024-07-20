a0d8d71a33ab9d70f280c9381868732e1a24b227
tuhao <tuhao@weipie.com>
2024-07-20 22:35:30 +0800

    feat: mason html

--- lua/mario/plugins/mason-lspconfig.lua
+++ lua/mario/plugins/mason-lspconfig.lua
@@ -5,7 +5,8 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		require("mason-lspconfig").setup({
-			ensure_installed = { "clangd", "pyright", "bashls", "lua_ls", "vimls", "jdtls" },
+			ensure_installed = { "clangd", "pyright", "bashls", "lua_ls", "vimls", "jdtls", "html" },
+			automatic_installation = true,
 		})
 	end,
 }
