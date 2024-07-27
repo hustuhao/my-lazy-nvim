a92f4cb5144f09341c96c095149744afc5410379
tuhao <tuhao@weipie.com>
2024-07-27 13:21:55 +0800

    refactor: modify toggleterm config

--- lua/mario/plugins/toggleterm.lua
+++ lua/mario/plugins/toggleterm.lua
@@ -1,44 +1,54 @@
 return {
-  
-    "akinsho/toggleterm.nvim",
-    cmd = { "ToggleTerm", "TermExec" },
+
+	"akinsho/toggleterm.nvim",
+	cmd = { "ToggleTerm", "TermExec" },
 	keys = {
-    {
-      "<leader>te",
-      function()
-        require("toggleterm.terminal").Terminal
-          :new({
-            direction = "float",
-          })
-          :toggle()
-      end,
-      desc = "Lazydocker",
-    },
-  },
-    opts = {
-      highlights = {
-        NormalNC = { link = "NormalNC" },
-        StatusLine = { link = "StatusLine" },
-        StatusLineNC = { link = "StatusLineNC" },
-        WinBar = { link = "WinBar" },
-        WinBarNC = { link = "WinBarNC" },
-      },
-      size = 10,
-      on_create = function()
-        vim.opt.foldcolumn = "0"
-        vim.opt.signcolumn = "no"
-      end,
-      -- shade_terminals = false,
-      -- shading_factor = 100,
-      direction = "float",
-      float_opts = { border = "rounded" },
-    },
+		{
+			"<leader>te",
+			function()
+				require("toggleterm.terminal").Terminal
+					:new({
+						direction = "float",
+					})
+					:toggle()
+			end,
+			desc = "Lazydocker",
+		},
+		{
+			"<leader>t",
+			function()
+				require("toggleterm.terminal").Terminal
+					:new({
+						direction = "horizontal",
+					})
+					:toggle()
+			end,
+			desc = "LazyHorizontalTerm",
+		},
+	},
+	opts = {
+		highlights = {
+			NormalNC = { link = "NormalNC" },
+			StatusLine = { link = "StatusLine" },
+			StatusLineNC = { link = "StatusLineNC" },
+			WinBar = { link = "WinBar" },
+			WinBarNC = { link = "WinBarNC" },
+		},
+		size = 10,
+		on_create = function()
+			vim.opt.foldcolumn = "0"
+			vim.opt.signcolumn = "no"
+		end,
+		-- shade_terminals = false,
+		-- shading_factor = 100,
+		direction = "float",
+		float_opts = { border = "rounded" },
+	},
 	config = function()
 		require("toggleterm").setup({
-		  shade_terminals = false,
-		  -- shell = "zsh --login",
-	   })
-		  -- key maps
-
-  end,
+			shade_terminals = false,
+			-- shell = "zsh --login",
+		})
+		-- key maps
+	end,
 }
