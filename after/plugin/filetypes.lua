30102f709e2a2b980500574bca72e255de6ef37a
tuhao <tuhao@weipie.com>
2024-07-28 19:11:59 +0800

    feat: add dockerls config and supprot for df file.

---
+++ after/plugin/filetypes.lua
@@ -0,0 +1,8 @@
+-- print("Loading filetypes.lua...")
+vim.filetype.add({
+	extension = {
+		df = "dockerfile",
+		MD = "markdown",
+	},
+})
+-- print("Filetype mapping added.")
--- lua/mario/plugins/mason-lspconfig.lua
+++ lua/mario/plugins/mason-lspconfig.lua
@@ -1,3 +1,28 @@
+local handlers = {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup({})
+	end,
+	-- Next, you can provide targeted overrides for specific servers.
+	["dockerls"] = function()
+		local lspconfig = require("lspconfig")
+		settings = {
+			docker = {
+				languageserver = {
+					formatter = {
+						ignoreMultilineInstructions = true,
+					},
+				},
+			},
+		}
+		lspconfig.dockerls.setup({
+			filetypes = { "dockerfile", "Dockerfile", "df" },
+		})
+	end,
+}
+
 return {
 
 	"williamboman/mason.nvim",
@@ -5,8 +30,9 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		require("mason-lspconfig").setup({
-			ensure_installed = { "clangd", "pyright", "bashls", "lua_ls", "vimls", "jdtls", "html" },
+			ensure_installed = { "clangd", "pyright", "bashls", "lua_ls", "vimls", "jdtls", "html", "dockerls" },
 			automatic_installation = true,
+			handlers = handlers,
 		})
 	end,
 }
