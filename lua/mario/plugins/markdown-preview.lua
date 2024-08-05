5b9dadd0550f3d121b0eae155c7a6fbce02182cc
tuhao <tuhao@weipie.com>
2024-08-06 00:11:34 +0800

    feat: add key mappings for markdown-preview,markdown plugins

--- lua/mario/plugins/markdown-preview.lua
+++ lua/mario/plugins/markdown-preview.lua
@@ -6,4 +6,16 @@ return {
 	build = function()
 		vim.fn["mkdp#util#install"]()
 	end,
+	config = function()
+		-- 设置快捷键来启用、禁用和切换 Markdown 预览
+
+		-- 启用 Markdown 预览
+		vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreview<CR>", { noremap = true, silent = true })
+
+		-- 停止 Markdown 预览
+		vim.api.nvim_set_keymap("n", "<leader>mps", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true })
+
+		-- 切换 Markdown 预览
+		vim.api.nvim_set_keymap("n", "<leader>mpt", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true })
+	end,
 }
--- lua/mario/plugins/markdown.lua
+++ lua/mario/plugins/markdown.lua
@@ -7,5 +7,17 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		config = function()
+			-- 设置快捷键来启用、禁用和切换插件
+
+			-- 启用 render-markdown 插件
+			vim.api.nvim_set_keymap("n", "<leader>me", ":RenderMarkdown enable<CR>", { noremap = true, silent = true })
+
+			-- 禁用 render-markdown 插件
+			vim.api.nvim_set_keymap("n", "<leader>md", ":RenderMarkdown disable<CR>", { noremap = true, silent = true })
+
+			-- 切换 render-markdown 插件
+			vim.api.nvim_set_keymap("n", "<leader>mt", ":RenderMarkdown toggle<CR>", { noremap = true, silent = true })
+		end,
 	},
 }
