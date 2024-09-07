f126e0243144ad60d8acdbb91bb1943f86378bfd
tuhao <tuhao@weipie.com>
2024-09-07 13:52:06 +0800

    feat: telescope,add git search,change preview window

--- lua/turato/plugins/telescope.lua
+++ lua/turato/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+	--		https://github.com/exosyphon/nvim/blob/main/lua/plugins/telescope.lua
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
@@ -7,8 +8,10 @@ return {
 			build = "make",
 			enabled = true,
 		},
-		{ "nvim-telescope/telescope-file-browser.nvim", enabled = true },
-		{ "nvim-telescope/telescope-dap.nvim",          enabled = true },
+		{ "nvim-telescope/telescope-file-browser.nvim",   enabled = true },
+		{ "nvim-telescope/telescope-dap.nvim",            enabled = true },
+		{ "aaronhallaert/advanced-git-search.nvim" },
+		{ "nvim-telescope/telescope-live-grep-args.nvim", enable = true },
 	},
 	branch = "0.1.x",
 	config = function()
@@ -20,14 +23,29 @@ return {
 				sorting_strategy = "ascending",
 				prompt_prefix = "🔍",
 				selection_caret = "» ",
-				-- layout_strategy = "horizontal",
+				layout_config = {
+					prompt_position = "bottom",
+					horizontal = {
+						width_padding = 0.04,
+						height_padding = 0.1,
+						preview_width = 0.5,
+						preview_cutoff = 0,
+					},
+					vertical = {
+						width_padding = 0.05,
+						height_padding = 0.1,
+						preview_height = 0.5,
+					},
+				},
+
+				layout_strategy = "horizontal",
 				-- layout_config = {
 				-- 	prompt_position = "bottom",
 				-- 	horizontal = {
-				-- 		width_padding = 0.04,
+				-- 		width_padding = 0.4,
 				-- 		height_padding = 0.1,
-				-- 		preview_width = 1,
-				-- 		preview_cutoff = 0,
+				-- 		preview_width = 0.1,
+				-- 		preview_cutoff = 0.1,
 				-- 	},
 				-- 	vertical = {
 				-- 		width_padding = 0.05,
@@ -37,22 +55,22 @@ return {
 				-- 	},
 				-- },
 				--
-				layout_strategy = "vertical",
-				layout_config = {
-					vertical = {
-						width = 0.9,
-						height = 0.9,
-						preview_height = 0.6,
-						preview_cutoff = 0,
-					},
-				},
+				-- layout_strategy = "vertical",
+				-- layout_config = {
+				-- 	vertical = {
+				-- 		width = 0.9,
+				-- 		height = 0.9,
+				-- 		preview_height = 0.6,
+				-- 		preview_cutoff = 0,
+				-- 	},
+				-- },
 				-- layout_strategy = "flex",
 				-- layout_config = {
 				-- 	flex = {
 				-- 		width = 0.9,
 				-- 	},
 				-- },
-				winblend = 0,  -- 窗口透明度
+				-- winblend = 0,  -- 窗口透明度
 				color_devicons = true, -- 使用彩色图标
 				set_env = { ["COLORTERM"] = "truecolor" },
 				-- file_previewer = require("telescope.previewers").vim_buffer_cat.new,
@@ -103,6 +121,8 @@ return {
 		telescope.load_extension("fzf")
 		telescope.load_extension("file_browser")
 		telescope.load_extension("dap")
+		telescope.load_extension("advanced_git_search")
+		telescope.load_extension("live_grep_args")
 		local builtin = require("telescope.builtin")
 
 		-- key maps
@@ -110,11 +130,25 @@ return {
 		local opts = { noremap = true, silent = true }
 
 		map("n", "-", ":Telescope file_browser<CR>")
-
 		map("n", "<leader>ff", builtin.find_files, opts) -- Lists files in your current working directory, respects .gitignore
 		map("n", "<leader>fx", builtin.treesitter, opts) -- Lists tree-sitter symbols
 		map("n", "<leader>fs", builtin.spell_suggest, opts) -- Lists spell options
-		-- map("n", "<leader>fg", builtin.live_grep, opts) -- live grep, use telescope-live-grep-args to replace it
+		map("n", "<leader>fw", builtin.grep_string, { desc = "Find Word under Cursor" })
+		map("n", "<leader>fgg", builtin.live_grep, opts) -- live grep, use telescope-live-grep-args to replace it
+		map("n", "<leader>fg", "<cmd>Telescope live_grep_args<cr>", { desc = "Live Grep" })
+		map("n", "<leader>fi", "<cmd>AdvancedGitSearch<CR>", { desc = "AdvancedGitSearch" })
+		map("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
+		map("n", "<leader>gbu", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
+		map("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+		map("n", "<leader>/", function()
+			-- You can pass additional configuration to telescope to change theme, layout, etc.
+			require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+				layout_config = { width = 0.7 },
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
+
 		map("n", "<leader>fh", builtin.help_tags, opts) -- help tags
 	end,
 }
